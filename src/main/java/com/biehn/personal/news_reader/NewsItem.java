6a79ece0bb347376bcf0da78bb9ed8f358f476b9
SooBiehn Hah <biehn.hah@gmail.com>
2026-08-23 13:27:53 +0900

    feat: Add NewsItem Class.

---
+++ src/main/java/com/biehn/personal/news_reader/NewsItem.java
@@ -0,0 +1,13 @@
+package com.biehn.personal.news_reader;
+
+public class NewsItem {
+  private String title;
+  private String description;
+  private String link;
+  private String imageUrl;
+  private String publishedAt;
+  private String author;
+  private String source;
+
+  public 
+}
