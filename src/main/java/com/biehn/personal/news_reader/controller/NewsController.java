a81ee954e683cdf23b131538003a628328f0fa9d
SooBiehn Hah <biehn.hah@gmail.com>
2026-08-25 15:08:39 +0900

    feat: add NewsController

---
+++ src/main/java/com/biehn/personal/news_reader/controller/NewsController.java
@@ -0,0 +1,21 @@
+package com.biehn.personal.news_reader.controller;
+
+import com.biehn.personal.news_reader.service.NewsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NewsController {
+  private final NewsService newsService;
+
+  public NewsController(NewsService newsService) {
+    this.newsService = newsService;
+  }
+
+  @GetMapping("/news")
+  public String news(Model model) throws Exception{
+    model.addAttribute("newsList",newsService.getNews());
+    return "news";
+  }
+}
