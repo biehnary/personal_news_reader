4c55ccc99d330000fd36eed3b10ab5947a9ec6d2
SooBiehn Hah <biehn.hah@gmail.com>
2026-08-31 13:32:50 +0900

    feat: add MK Global RSS source

---
+++ src/main/java/com/biehn/personal/news_reader/source/MkGlobalSource.java
@@ -0,0 +1,77 @@
+package com.biehn.personal.news_reader.source;
+
+import com.biehn.personal.news_reader.model.NewsItem;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+@Component
+public class MkGlobalSource implements NewsSource {
+
+  @Override
+  public String getSourceName() {
+    return "MK Global";
+  }
+
+  @Override
+  public String getRssUrl() {
+    return "https://www.mk.co.kr/rss/30300018/";
+  }
+
+  @Override
+  public List<NewsItem> extract(Document document) {
+    //item(title, link, description, pubDate, author, media:content)
+    List<NewsItem> newsItems = new ArrayList<>();
+
+    NodeList itemNodes = document.getElementsByTagName("item");
+
+    for (int i = 0; i < itemNodes.getLength(); i++) {
+      Node itemNode = itemNodes.item(i);
+      Element itemElement = (Element) itemNode;
+      String imageUrl = null;
+
+      Node titleNode = itemElement.getElementsByTagName("title")
+          .item(0);
+      if (titleNode == null) {
+        continue;
+      }
+      String title = titleNode.getTextContent();
+
+      Node linkNode = itemElement.getElementsByTagName("link")
+          .item(0);
+      if (linkNode == null) {
+        continue;
+      }
+      String link = linkNode.getTextContent();
+
+      Node descriptionNode = itemElement.getElementsByTagName("description")
+          .item(0);
+      String description = descriptionNode == null ? null : descriptionNode.getTextContent();
+
+      Node pubDateNode = itemElement.getElementsByTagName("pubDate")
+          .item(0);
+      String publishedAt = pubDateNode == null ? null : pubDateNode.getTextContent();
+
+      Node authorNode = itemElement.getElementsByTagName("author")
+          .item(0);
+      String author = authorNode == null ? null : authorNode.getTextContent();
+
+      Node mediaNode = itemElement.getElementsByTagName("media:content")
+          .item(0);
+      if (mediaNode != null) {
+        Element mediaElement = (Element) mediaNode;
+        imageUrl = mediaElement.getAttribute("url");
+      }
+
+      NewsItem newsItem = new NewsItem(title, description, link, imageUrl, publishedAt, author,
+          "MK Global");
+      newsItems.add(newsItem);
+    }
+
+    return newsItems;
+  }
+}
